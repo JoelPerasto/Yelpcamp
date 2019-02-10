c98091ca6a5231ac6415794b062dc02ce2e77722
Joel <joel.perasto@gmail.com>
2019-02-10 11:30:46 +0000

    Initial commit

--- app.js
+++ app.js
@@ -18,8 +18,7 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes          = require("./routes/index");
     
-var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v14";
-mongoose.connect(url);
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v14", {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
